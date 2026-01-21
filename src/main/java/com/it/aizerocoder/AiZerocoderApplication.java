e71a1edb3405009677a655c8f89bd02ae84c00d2
wanghf <wang.h.f@outlook.com>
2026-01-21 18:12:17 +0800

    后端项目初始化

---
+++ src/main/java/com/it/aizerocoder/AiZerocoderApplication.java
@@ -0,0 +1,13 @@
+package com.it.aizerocoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiZerocoderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiZerocoderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/it/aizerocoder/AiZerocoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it.aizerocoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiZerocoderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
