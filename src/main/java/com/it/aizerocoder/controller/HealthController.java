76f6290cbedbeb018bc29de9abf136fa81c439f6
wanghf <wang.h.f@outlook.com>
2026-01-21 22:01:22 +0800

    feat(common): 添加基础响应类和自定义异常处理机制,配置 CorsConfig 跨域配置，允许所有来源访问

--- src/main/java/com/it/aizerocoder/controller/HealthController.java
+++ src/main/java/com/it/aizerocoder/controller/HealthController.java
@@ -1,5 +1,7 @@
 package com.it.aizerocoder.controller;
 
+import com.it.aizerocoder.common.BaseResponse;
+import com.it.aizerocoder.common.ResultUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
 
     @GetMapping("/")
-    public String health(){
-        return "ok";
+    public BaseResponse<String> health(){
+        return ResultUtils.success("ok");
     }
 }
